d4445306bbe4475ae2b9dc25649e4ddc5acb5630
Nasir-afridi <nasirwc05@gmail.com>
2025-07-29 16:56:51 +0500

    Import the button and card component and passing data into it

--- Learning-React/src/App.jsx
+++ Learning-React/src/App.jsx
@@ -1,11 +1,15 @@
 import Welcome from "./Welcome"
 import Greeting from "./Greeting"
+import Button from "./02-Button"
+import Card from "./03-Card"
 function App() {
 
   return (
     <>
       <Welcome />
       <Greeting />
+      <Button label="Hello this is me" onClick={() => alert("hello")}/>
+      <Card title = "Bottle" description = "this is a bottle of silver"/>
     </>
   )
 }
