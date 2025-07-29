0c4a4164539b464e5223bce8f4f61006acf6e10f
Nasir-afridi <nasirwc05@gmail.com>
2025-07-29 17:17:06 +0500

    added he multiple cards and buttons

--- Learning-React/src/App.jsx
+++ Learning-React/src/App.jsx
@@ -8,8 +8,12 @@ function App() {
     <>
       <Welcome />
       <Greeting />
-      <Button label="Hello this is me" onClick={() => alert("hello")}/>
-      <Card title = "Bottle" description = "this is a bottle of silver"/>
+      <Button label="Hello this is me" onClick={() => alert("hello this is bootle")}/>
+      <Button label="Hello this is shoes" onClick={() => alert("hello this is shoes")}/>
+      <Button label="Hello this is pc" onClick={() => alert("hello this is pc")}/>
+      <Card title = "Bottle" description = "this is a bottle of silver" buttonLabel = "click here" onOurClick={() => alert("Buying Bottle")} />
+      <Card title = "shoes" description = "this is a shoes of nike" buttonLabel = "click here" onOurClick={() => alert("Buying shoes")} />
+      <Card title = "pc" description = "this is a pc of windows" buttonLabel = "click here"  onOurClick={() => alert("Buying pc")} />
     </>
   )
 }
