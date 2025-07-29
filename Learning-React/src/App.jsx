ae75618a082123350b20fb190aad3ee8bf7a5561
Nasir-afridi <nasirwc05@gmail.com>
2025-07-29 14:48:17 +0500

    added the components

---
+++ Learning-React/src/App.jsx
@@ -0,0 +1,13 @@
+import Welcome from "./Welcome"
+import Greeting from "./Greeting"
+function App() {
+
+  return (
+    <>
+      <Welcome />
+      <Greeting />
+    </>
+  )
+}
+
+export default App
