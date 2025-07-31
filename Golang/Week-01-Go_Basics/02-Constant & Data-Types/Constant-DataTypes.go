67ed2f1a193fc5ca6062f0717a626ad8368495b1
Nasir-afridi <nasirwc05@gmail.com>
2025-07-31 14:10:23 +0500

    Day-2 Constants & Data types done

---
+++ Golang/Week-01-Go_Basics/02-Constant & Data-Types/Constant-DataTypes.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Muhammad Nasir"
+	var age int = 20
+	var favLanguage string = "Pashto"
+	var height float32 = 5.10
+	var isStudent = true
+
+	Name := "Nasir"
+	Age := 22
+	Language := "pashto"
+	Height := 5.10
+	Student := true
+
+	const Country = "pakistan"
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(favLanguage)
+	fmt.Println(height)
+	fmt.Println(isStudent)
+	fmt.Println(Age)
+	fmt.Println(Language)
+	fmt.Println(Name)
+	fmt.Println(Student)
+	fmt.Println(Height)
+	fmt.Println(Country)
+
+}
