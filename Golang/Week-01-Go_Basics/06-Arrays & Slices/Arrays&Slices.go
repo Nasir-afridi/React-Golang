c3487358fdefa3697b3154dcd99b60defa34ab4f
Nasir-afridi <nasirwc05@gmail.com>
2025-07-31 19:23:25 +0500

    added the task-1 created a array of integers and print it

--- Golang/Week-01-Go_Basics/06-Arrays & Slices/Arrays&Slices.go
+++ Golang/Week-01-Go_Basics/06-Arrays & Slices/Arrays&Slices.go
@@ -15,4 +15,8 @@ func main() {
 	fmt.Println(country)
 	fmt.Println(len(country)) //checking the length of array.
 	fmt.Println(cap(country))
+
+	//task-1
+	integer := [5]int{1, 3, 4, 5, 6}
+	fmt.Println(integer)
 }
