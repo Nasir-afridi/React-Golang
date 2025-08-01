ca539166a60cfada8b85ecb39601dae2268d1eb4
Nasir-afridi <nasirwc05@gmail.com>
2025-08-01 15:32:01 +0500

    task-2 added the new student to map

--- Golang/Week-01-Go_Basics/07-Maps/map.go
+++ Golang/Week-01-Go_Basics/07-Maps/map.go
@@ -5,12 +5,16 @@ import "fmt"
 // Go mein map ek built-in data type hai jo keys ke against values ko store karta hai
 // bilkul JavaScript ke object ya dictionary ki tarah.
 func main() {
-	//Task-1
 
+	//Task-1
 	students := map[string]string{
 		"Nasir": "Islamabad",
 		"Ahmad": "Karachi",
 		"Mirza": "wah cantt",
 	}
+	fmt.Println(students)
+
+	//Task-2
+	students["sadeeq"] = "Sindh"
 	fmt.Println(students)
 }
