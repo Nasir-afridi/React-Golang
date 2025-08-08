e17cc1cc81449bf958aea4f5fd83ac5eeb01de3b
Nasir-afridi <nasirwc05@gmail.com>
2025-08-08 16:05:32 +0500

    declare a empty variable and initialize it

---
+++ Golang/Week-01-Basics/01_variables/variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// empty typed variable declaration. first declare and then initialize. ham agr value na dain to ye by default value layga.
+	var name string
+	name = "Nasir"
+	fmt.Println(name)
+	name = "ali"
+	fmt.Println(name)
+
+}
