3d7e93b87b364fe3e4270cecd8847eb83b2870f0
Nasir-afridi <nasirwc05@gmail.com>
2025-08-08 16:34:24 +0500

    task 4 print the city and year using shortcut

--- Golang/Week-01-Basics/Task/Task.go
+++ Golang/Week-01-Basics/Task/Task.go
@@ -19,4 +19,9 @@ func main() {
 	// Pow ka mtlb hai ktne power raise krna hai first one base nmbr second one power raise.
 	area := math.Pi * math.Pow(radius, 2)
 	fmt.Println("the area of the circle is ", area)
+
+	// 4.
+	city := "Islamabad"
+	year := 2025
+	fmt.Println(city, year)
 }
