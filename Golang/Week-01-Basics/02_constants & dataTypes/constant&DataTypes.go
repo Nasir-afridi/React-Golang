843d6e4cb451a84eb08ef570323ea844c2946466
Nasir-afridi <nasirwc05@gmail.com>
2025-08-08 16:52:35 +0500

    basic types added

--- Golang/Week-01-Basics/02_constants & dataTypes/constant&DataTypes.go
+++ Golang/Week-01-Basics/02_constants & dataTypes/constant&DataTypes.go
@@ -10,4 +10,31 @@ func main() {
 	// Typed constant : iski type user khud define kryga
 	const NAME string = "Nasir"
 	fmt.Println(NAME)
+
+	// ***Data Types*** two types of data types in go basic and derieved types
+
+	// 1: Basic types
+
+	// Integer
+	var age int = 25
+	fmt.Println("Age:", age)
+
+	// Float
+	var price float64 = 99.99
+	fmt.Println("Price:", price)
+
+	// Boolean
+	var isLoggedIn bool = true
+	fmt.Println("Is Logged In:", isLoggedIn)
+
+	// String
+	var country string = "Pakistan"
+	fmt.Println("Country:", country)
+
+	// Zero values example
+	var num int
+	var status bool
+	var text string
+	fmt.Println("Zero values => num:", num, ", status:", status, ", text:", text)
+
 }
