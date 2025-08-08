766f241386840d362a614c2b9358b278cc5167f6
Nasir-afridi <nasirwc05@gmail.com>
2025-08-08 16:47:23 +0500

    Typed variable declaration

--- Golang/Week-01-Basics/02_constants & dataTypes/constant&DataTypes.go
+++ Golang/Week-01-Basics/02_constants & dataTypes/constant&DataTypes.go
@@ -6,4 +6,8 @@ func main() {
 	// Untyped constant : iski type go khud guess krlyga
 	const PI = 3.14
 	fmt.Println(PI)
+
+	// Typed constant : iski type user khud define kryga
+	const NAME string = "Nasir"
+	fmt.Println(NAME)
 }
