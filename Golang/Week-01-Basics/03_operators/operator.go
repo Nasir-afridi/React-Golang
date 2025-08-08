59b477625e24e407ef92e0a165bc0cbe4c3f3c00
Nasir-afridi <nasirwc05@gmail.com>
2025-08-08 17:31:21 +0500

    added the arthematic operators

---
+++ Golang/Week-01-Basics/03_operators/operator.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Arithmetic
+	a := 10
+	b := 3
+	fmt.Println("Addition:", a+b)
+	fmt.Println("Subtraction:", a-b)
+	fmt.Println("Multiplication:", a*b)
+	fmt.Println("Division:", a/b)
+	fmt.Println("Modulus:", a%b)
+
+}
