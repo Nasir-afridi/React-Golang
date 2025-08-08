1c76ab6b5444bdfadcb0b768dbda60ac8a3d46db
Nasir-afridi <nasirwc05@gmail.com>
2025-08-08 16:45:56 +0500

    unTyped constant initialize

--- Golang/Week-01-Basics/02_constants & dataTypes/constant&DataTypes.go
+++ Golang/Week-01-Basics/02_constants & dataTypes/constant&DataTypes.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// Typed constant : iski type go khud guess krlyga
+	// Untyped constant : iski type go khud guess krlyga
 	const PI = 3.14
 	fmt.Println(PI)
 }
