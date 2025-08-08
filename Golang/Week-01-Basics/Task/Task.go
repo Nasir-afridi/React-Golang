298c740665ad0b939fad5dfbb705837642d7ad33
Nasir-afridi <nasirwc05@gmail.com>
2025-08-08 16:32:03 +0500

    calculated the area of the circle

--- Golang/Week-01-Basics/Task/Task.go
+++ Golang/Week-01-Basics/Task/Task.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	// 1.
@@ -10,4 +13,10 @@ func main() {
 	// 2.
 	var a, b, c int = 1, 2, 3
 	fmt.Println(a + b + c)
+
+	//3.
+	var radius float64 = 5
+	// Pow ka mtlb hai ktne power raise krna hai first one base nmbr second one power raise.
+	area := math.Pi * math.Pow(radius, 2)
+	fmt.Println("the area of the circle is ", area)
 }
