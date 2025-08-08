70e489a0fa51a301002f77452b16a69d57826908
Nasir-afridi <nasirwc05@gmail.com>
2025-08-08 18:55:35 +0500

    Added the for loop

---
+++ Golang/Week-01-Basics/05_Loops/loops.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+//For loop ek control structure hai jo repeatedly koi code block execute karta hai jab tak condition true ho.
+
+func main() {
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+}
