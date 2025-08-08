09e511c368960b6e6a14a0298880f09ea4391899
Nasir-afridi <nasirwc05@gmail.com>
2025-08-08 17:49:18 +0500

    added the task one arthematic operator

---
+++ Golang/Week-01-Basics/03_operators/Task/task.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 1.
+	a := 10
+	b := 20
+
+	fmt.Println(a + b)
+	fmt.Println(a == b)
+	fmt.Println(a * b)
+	fmt.Println(a / b)
+	fmt.Println(b % a)
+}
