2e286843994e4d116b3ba034801734ab0b294bd1
Nasir-afridi <nasirwc05@gmail.com>
2025-08-08 19:24:43 +0500

    added the taask to break when infinite loop come to 5

--- Golang/Week-01-Basics/05_Loops/Task/task.go
+++ Golang/Week-01-Basics/05_Loops/Task/task.go
@@ -12,4 +12,20 @@ func main() {
 	for a := 2; a >= 10; a += 2 {
 		fmt.Println(a)
 	}
+
+	// 3.
+	arr := [3]int{1, 3, 4}
+	for x := 0; x < len(arr); x++ {
+		fmt.Println(arr[x])
+	}
+
+	// 4.
+	b := 1
+	for {
+		fmt.Println(b)
+		if b == 5 {
+			break
+		}
+		b++
+	}
 }
